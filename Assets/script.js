90e44e9fb37abfe6cd036a50e00f387ea579350b
Nate McMahon <nathaniel.mcmahon@hrblock.com>
2023-10-11 21:08:01 -0500

    create list elements for question choices, cycle through all our questions, congratulations upon completion

--- Assets/script.js
+++ Assets/script.js
@@ -1,12 +1,39 @@
+var body = document.body;
 var score = 0;
 var i = 1;
+
+var listEl = document.createElement("ol");
+var li1 = document.createElement("li");
+var li2 = document.createElement("li");
+var li3 = document.createElement("li");
+// var li4 = document.createElement("li");
 
 var startButton = document.querySelector(".start-button");
 startButton.addEventListener("click", startQuiz);
 
 var prompt = document.querySelector("#prompt");
 
-var questions = ["what's your favorite color?", "how is your day going?", "where are you from?"];
+var options = document.querySelector("#optionsList");
+
+const questionsObjArr = [
+    {
+        question: "what's your favorite color?",
+        choices: ["blue", "green", "red", "yellow"],
+        answer: "green"
+    },
+    {
+        question: "how is your day going?",
+        choices: ["good", "okay", "bad"],
+        answer: "good"
+    },
+    {
+        question: "where are you from?",
+        choices: ["Kansas City", "Chicago", "India"],
+        answer: "Chicago"
+    }
+  ];
+
+const questions = ["what's up", "how are you", "what's cookin good lookin"];
 
 var nextButton = document.querySelector(".next-button");
 nextButton.addEventListener("click", nextQuestion);
@@ -16,9 +43,22 @@ function startQuiz() {
     timerCount = 75;
     // Prevents start button from being clicked when round is in progress
     // startButton.disabled = true;
-    prompt.textContent = questions[0];
+    prompt.textContent = questionsObjArr[0].question;
     // startTimer();
+    startButton.style.display = "none";
+    nextButton.style.display = "inline";
     
+    
+    
+
+    options.appendChild(listEl);
+    listEl.appendChild(li1);
+    listEl.appendChild(li2);
+    listEl.appendChild(li3);
+
+    li1.textContent = questionsObjArr[0].choices[0];
+    li2.textContent = questionsObjArr[0].choices[1];
+    li3.textContent = questionsObjArr[0].choices[2];
   }
 
 function startTimer() {
@@ -49,9 +89,16 @@ function renderQuestion() {
 }
 
 function nextQuestion() {
-    prompt.textContent = questions[i];
-    i++;
-    if (i > questions.length) {
+    if (i > 2) {
         prompt.textContent = "You've answered all my questions, good job!";
+        return;
     }
+    prompt.textContent = questionsObjArr[i].question;
+        
+    li1.textContent = questionsObjArr[i].choices[0];
+    li2.textContent = questionsObjArr[i].choices[1];
+    li3.textContent = questionsObjArr[i].choices[2];
+    
+    i++;
+    
 }
